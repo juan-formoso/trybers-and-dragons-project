bbae7869104b476ece01f84d9c13c05aefb3b66f
Juan Formoso <trajano.juan@gmail.com>
2022-03-14 00:08:54 -0300

    Add elf.ts

---
+++ src/Races/Elf.ts
@@ -0,0 +1,18 @@
+import Race from './Race';
+
+abstract class Elf extends Race {
+  private _maxLifePoints: number;
+  static _createdRacesInstances = 0;
+
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+    this._maxLifePoints = 99;
+    Elf._createdRacesInstances += 1;
+  }
+
+  static createdRacesInstances(): number {
+    return Elf._createdRacesInstances;
+  }
+}
+
+export default Elf;
