99d9faacf8789a3121f5fa904c7df7cc577391f4
Juan Formoso <trajano.juan@gmail.com>
2022-03-14 00:29:24 -0300

    Add orc.ts

---
+++ src/Races/Orc.ts
@@ -0,0 +1,18 @@
+import Race from './Race';
+
+abstract class Orc extends Race {
+  private _maxLifePoints: number;
+  static _createdRacesInstances = 0;
+
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+    this._maxLifePoints = 74;
+    Orc._createdRacesInstances += 1;
+  }
+
+  static createdRacesInstances(): number {
+    return Orc._createdRacesInstances;
+  }
+}
+
+export default Orc;
