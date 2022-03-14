fe9b9095edf78656802e76b59cbe13c040141d38
Juan Formoso <trajano.juan@gmail.com>
2022-03-14 17:24:15 -0300

    Retira comentários de Race.ts

--- src/Races/Race.ts
+++ src/Races/Race.ts
@@ -1,31 +1,3 @@
-/* A classe Race deve ter os atributos privados: name e dexterity, ambos inicializados em seu construtor;
-
-O atributo name dever ser do tipo string;
-
-O atributo dexterity dever ser do tipo number;
-name e dexterity devem ser recebidos como parâmetro e inicializados no construtor;
-
-Os atributos da classe Race podem ser lidos mas não podem ser alterados:
-name deve retornar o tipo string;
-dexterity deve retornar o tipo number;
-
-A classe Race deve ter um método estático chamado createdRacesInstances que retorna um number;
-
-Esse número é correspondente a quantidade de instâncias criadas a partir das classes estendidas da classe Race;
-
-Cada raça vai ter seu número máximo de instâncias e isto será feito dentro de suas classes especializadas;
-
-Na classe Race será lançada apenas a mensagem de erro: Not implemented;
-
-A classe Race deve ter um getter abstrato chamado maxLifePoints que retorna um number;
-
-Esse número corresponde à quantidade máxima de pontos de vida da raça.
-
-Cada raça vai ter seu número máximo de pontos e isto será feito dentro de suas classes especializadas;
-
-Na classe Race deve estar apenas a assinatura do método;
- */
-
 abstract class Race {
   private _name: string;
   private _dexterity: number;
