62f9b4063856717f5ad901f8643720c1f562bec4
Juan Formoso <trajano.juan@gmail.com>
2022-03-14 23:35:12 -0300

    Add Character.ts

---
+++ src/Character.ts
@@ -0,0 +1,32 @@
+import Archetype, { Mage } from './Archetypes';
+import Energy from './Energy';
+import Fighter from './Fighter';
+import Race, { Elf } from './Races';
+import getRandomInt from './utils';
+
+class Character implements Fighter {
+  protected _race: Race;
+  protected _archetype: Archetype;
+  protected _maxLifePoints: number;
+  protected _lifePoints: number;
+  protected _strength: number;
+  protected _defense: number;
+  protected _dexterity: number;
+  protected _energy: Energy;
+
+  constructor(name: string) {
+    this._race = new Elf(name, 50);
+    this._archetype = new Mage(name);
+    this._maxLifePoints = this._race.maxLifePoints / 2;
+    this._lifePoints = this._maxLifePoints;
+    this._strength = getRandomInt(1, 10);
+    this._defense = getRandomInt(1, 10);
+    this._dexterity = this._race.dexterity;
+    this._energy = {
+      type_: this._archetype.energyType,
+      amount: getRandomInt(1, 10),
+    };
+  }
+}
+
+export default Character;
