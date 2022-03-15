2fb7f30e25713a7cdcdd23d5b0f72eb44aa552ce
Juan Formoso <trajano.juan@gmail.com>
2022-03-14 21:23:27 -0300

    Add Mage.ts

---
+++ src/Archetypes/Mage.ts
@@ -0,0 +1,23 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+class Mage extends Archetype {
+  _energyType: EnergyType;
+  static _createdArchetypesInstances = 0;
+
+  constructor(name: string) {
+    super(name);
+    this._energyType = 'mana';
+    Mage._createdArchetypesInstances += 1;
+  }
+
+  get energyType(): EnergyType {
+    return this._energyType;
+  }
+
+  static createdArchetypesInstances(): number {
+    return Mage._createdArchetypesInstances;
+  }
+}
+
+export default Mage;
