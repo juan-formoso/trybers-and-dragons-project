14c9a1327132f104c3f818e7a3523df2e153493d
Juan Formoso <trajano.juan@gmail.com>
2022-03-14 21:51:43 -0300

    Update index.ts

--- src/Archetypes/index.ts
+++ src/Archetypes/index.ts
@@ -1,3 +1,13 @@
 import Archetype from './Archetype';
+import Ranger from './Ranger';
+import Mage from './Mage';
+import Necromancer from './Necromancer';
+import Warrior from './Warrior';
 
 export default Archetype;
+export {
+  Ranger,
+  Mage,
+  Necromancer,
+  Warrior,
+};
